5e4386e5baaaa66d37ec9112a7eb5204bfe7e7d5
Daniel Carpio <daniel.carpio@seidor.com>
2024-11-09 02:31:35 +0000

    First commit

---
+++ sapui5app/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sdr/sapui5app/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ sapui5app/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sdr/sapui5app/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
